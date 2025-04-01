d4a79414f2b169e10c18c830fff573b18f4400a7
purvaakshita <purvaakshita@gmail.com>
2025-04-01 14:31:14 +0530

    new

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import { appContext } from '../App'
+import { useContext } from 'react'
 export default function Cart() {
+  const {Cart,products}=useContext(appContext)
   return (
-    <div>Cart</div>
+    <div>
+      {products.map(value=>(
+        <div>{value.name}</div>
+      ))}
+    </div>
   )
+
 }
