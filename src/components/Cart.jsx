bf30840de3932c6fad7094b648cac64242ab9fae
purvaakshita <purvaakshita@gmail.com>
2025-04-03 09:58:47 +0530

    added

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import { appContext } from '../App'
-import { useContext } from 'react'
+import React from "react";
+import { appContext } from "../App";
+import { useContext } from "react";
 export default function Cart() {
-  const {Cart,products}=useContext(appContext)
+  const { cart, setCart, products } = useContext(appContext);
+  const handleDelete = (id) => {
+    setCart({ ...cart, [id]: 0 });
+  };
+  
   return (
     <div>
-      {products.map(value=>(
-        <div>{value.name}</div>
-      ))}
+      {products.map(
+        (value) =>
+          cart[value.id]>0 && (
+            <div key={value.id}>
+              {value.name}-{value.price}-<button>-</button>{cart[value.id]}<button>+</button>-
+              {value.price * cart[value.id]}-
+              <button onClick={() => handleDelete(value.id)}>Delete</button>
+            </div>
+          )
+      )}
     </div>
-  )
-
+  );
 }
